33e70e82f266a32ab2b9b7cd6e1f36279d984e8c
p6l-richard <richard.poelderl@gmail.com>
2022-06-30 20:59:39 -0500

    use webext-bridge

--- background.ts
+++ background.ts
@@ -1,65 +1,38 @@
-chrome.action.onClicked.addListener((currentTab) => {
-  if (!currentTab?.id) {
+import { sendMessage } from "webext-bridge"
+
+// -- Send event to content-script on action click --
+
+// Types
+export interface HandleActionClick {
+  key: "showModal"
+  payload: { message: string }
+  response: { success: true }
+}
+// Send event
+chrome.action.onClicked.addListener(async (currentTab) => {
+  // early return if tab is inactive or doesn't have an id
+  if (!currentTab?.id || !currentTab.active) {
     return
   }
-  chrome.tabs.sendMessage(currentTab?.id, "HI FROM BG", (response) =>
-    console.log({ response })
-  )
-})
 
-// PING-PONG
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log({ request })
-  if (request === "ping") {
-    sendResponse(true)
-  }
-  return true
+  // 🚨 GOTCHA: Breaks after reloading extension
+  // The user needs to reload tabs/pages for this to work
+  // note: it doesnt even provide chrome.runtime.lastError if used with webext-bridge
+
+  // send message from background to content
+  const res = await sendMessage<
+    HandleActionClick["response"],
+    HandleActionClick["key"]
+  >(
+    "showModal",
+    { message: "action was clicked" },
+    {
+      context: "content-script",
+      tabId: currentTab.id
+    }
+  )
+  // ... after content responded, log response to background's console
+  console.log("response received - modal showed? ", res)
 })
-// background console:
-// > {response: undefined}
-// Unchecked runtime.lastError: Could not establish connection. Receiving end does not exist.
 
 export {}
-
-// chrome.action.onClicked.addListener(async (currentTab) => {
-//   chrome.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-//     const tabId = tabs[0].id
-//   })
-// })
-
-// PING the content script to avoid lastError: receiving end not ready
-// let tab: chrome.tabs.Tab
-// function pingAndConnect() {
-//   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//     tab = Boolean(tabs[0]?.id) ? tabs[0] : tab
-
-//     if (tab.id) {
-//       chrome.tabs.sendMessage(tab.id, { message: "ping" }, (response) => {
-//         console.log("err?", chrome.runtime.lastError)
-//         if (chrome.runtime.lastError) {
-//           setTimeout(pingAndConnect, 1000)
-//         } else {
-//           var port = chrome.tabs.connect(tab.id, { name: "hi" })
-//           console.log("connected to port:", port.name)
-//           port.postMessage({ greeting: "hello" })
-//           port.onMessage.addListener((response) => console.log({ response }))
-//         }
-//       })
-//     }
-//   })
-// }
-
-// chrome.action.onClicked.addListener(async (currentTab) => {
-//   pingAndConnect()
-// })
-
-// chrome.runtime.onConnect.addListener((port) => {
-//   port.onMessage.addListener((msg) => {
-//     // Handle message however you want
-//     console.log("Not sure when this is triggered?")
-//   })
-// })
-
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) =>
-//   sendResponse("pong")
-// )
--- content.ts
+++ content.ts
@@ -1,34 +1,19 @@
 import type { PlasmoContentScript } from "plasmo"
+import { onMessage } from "webext-bridge"
+
+import type { HandleActionClick } from "~background"
 
 export const config: PlasmoContentScript = {
   matches: ["https://*/*"]
 }
 
-let hasConnection = false
-// THIS IS NOW TOP-LEVEL
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log(
-    sender.tab
-      ? "from a content script:" + sender.tab.url
-      : "from the extension"
-  )
-  console.log({ request })
-  sendResponse("HI from content")
-  return true
-})
-
-// NO LOGS ?
-
-function ping() {
-  if (hasConnection) return
-  chrome.runtime.sendMessage("ping", (response) => {
-    console.log({ response })
-    if (chrome.runtime.lastError) {
-      setTimeout(ping, 1000)
-    } else {
-      hasConnection = response
-    }
-  })
-}
-
-ping()
+// Handle action click in content-script
+onMessage<HandleActionClick["payload"], HandleActionClick["key"]>(
+  "showModal",
+  ({ data }) => {
+    // alert user in tab about action click
+    alert(data.message)
+    // return response to background script
+    return { success: true }
+  }
+)
