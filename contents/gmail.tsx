f50f50ba80f4d1e32454caa7dfba0e30425b3b02
p6l-richard <richard.poelderl@gmail.com>
2022-07-04 15:58:49 -0300

    WIP: add chainable promise retries

--- contents/gmail.tsx
+++ contents/gmail.tsx
@@ -1,4 +1,5 @@
 import type { PlasmoContentScript } from "plasmo"
+import icon from "data-base64:~assets/icon512.png"
 
 export const config: PlasmoContentScript = {
   matches: ["https://mail.google.com/mail/u/0/*"] // test on: https://mail.google.com/mail/u/0/#inbox?compose=new
@@ -6,25 +7,61 @@ export const config: PlasmoContentScript = {
 
 // PROGRAMATICALLY INJECT EXTENSION CONTAINER & MOUNT
 
-// select .btC class for the google compose window's bottom sheet
-export const getMountPoint = async () => {
-  // get all compose windows (user may have multiple open at once)
-  const composeWindowHotbars = document.querySelectorAll(".btC")
-  // inject item in every hotbar
-  for (const hotbar of composeWindowHotbars) {
-    // only if not already injected
-    if (!hotbar.querySelector("#agreeto-item")) {
-      // store send button next to which to insert out item
-      const sendButton = hotbar.childNodes.item(0)
-      // create our item as a div w/ custom id
-      const container = document.createElement("div")
-      container.id = "agreeto-item"
-      // insert item
-      sendButton?.parentNode?.insertBefore(container, sendButton.nextSibling)
-    }
+const observer = new MutationObserver(() => {
+
+// get all compose windows (user may have multiple open at once)
+const composeWindowHotbars = document.querySelectorAll(".btC")
+// inject item in every hotbar
+for (const hotbar of composeWindowHotbars) {
+  // only if not already injected
+  if (!hotbar.querySelector("#agreeto-item")) {
+    // store send button next to which to insert out item
+    const sendButton = hotbar.childNodes.item(0)
+    // create our item as a div w/ custom id
+    const container = document.createElement("div")
+    container.id = "agreeto-item"
+    // insert item
+    sendButton?.parentNode?.insertBefore(container, sendButton.nextSibling)
   }
+}
+})
+var RETRY_INTERVAL = 500;
+
+const rejectDelay = (reason) => {
+    return new Promise(function(resolve, reject) {
+        setTimeout(reject.bind(null, reason), RETRY_INTERVAL); 
+    });
+}
+
+// const waitAndReturnAgreeToItem = () => {
+//   const agreeToItem = document.querySelector("#agreeto-item")
+//   console.log("[waitAndReturnAgreeToItem] ", agreeToItem)
+//   if (!agreeToItem) {
+//     setTimeout(() => waitAndReturnAgreeToItem(), 1000)
+//   }
+//   return agreeToItem
+// }
+let attemptCount = 0
+const attempt = async () => {
+  attemptCount++
+  console.log("attempt ",attemptCount)
+  const agreeToItem = document.querySelector("#agreeto-item")
+  if (!agreeToItem) throw Error("Element with id #agreeto-item not found.")
+  return agreeToItem
+}
+export const getMountPoint = async () => {
+  observer.observe(document.body, { childList: true });
+  const MAX_RETRIES = 5;
+  let promise = Promise.reject();
+  let result: HTMLElement = null;
+  // for range
+  [...Array(MAX_RETRIES)].map((_,i) => {
+    promise = promise.catch(attempt).catch(rejectDelay)
+  });
+  promise = promise.then((res) => {console.log("res: ", res); result = res}).catch((err) => console.error(err))
   // return inserted div to mount react
-  return document.querySelector("#agreeto-item")
+  console.log("after awaited function")
+  return result
 }
 
 // REACT COMPONENT TO MOUNT
@@ -42,7 +79,7 @@ const GmailItem = () => {
         }}>
         <img
           title="agreeto-logo"
-          // src={chrome.runtime.getURL("item-icon.svg")}
+          src={icon}
           style={{ width: "28px", height: "28px" }}
         />
       </button>
