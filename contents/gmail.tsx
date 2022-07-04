9613918cc688f50f5f6ecae67a2b5d8dcccdf96f
p6l-richard <richard.poelderl@gmail.com>
2022-07-04 17:21:40 -0300

    ✅ add getRootContainer function w/ waitForElements helper

--- contents/gmail.tsx
+++ contents/gmail.tsx
@@ -1,67 +1,10 @@
 import type { PlasmoContentScript } from "plasmo"
 import icon from "data-base64:~assets/icon512.png"
+import { ReactElement, useEffect } from "react";
+import { createRoot } from "react-dom/client";
 
 export const config: PlasmoContentScript = {
   matches: ["https://mail.google.com/mail/u/0/*"] // test on: https://mail.google.com/mail/u/0/#inbox?compose=new
-}
-
-// PROGRAMATICALLY INJECT EXTENSION CONTAINER & MOUNT
-
-const observer = new MutationObserver(() => {
-
-// get all compose windows (user may have multiple open at once)
-const composeWindowHotbars = document.querySelectorAll(".btC")
-// inject item in every hotbar
-for (const hotbar of composeWindowHotbars) {
-  // only if not already injected
-  if (!hotbar.querySelector("#agreeto-item")) {
-    // store send button next to which to insert out item
-    const sendButton = hotbar.childNodes.item(0)
-    // create our item as a div w/ custom id
-    const container = document.createElement("div")
-    container.id = "agreeto-item"
-    // insert item
-    sendButton?.parentNode?.insertBefore(container, sendButton.nextSibling)
-  }
-}
-})
-var RETRY_INTERVAL = 500;
-
-const rejectDelay = (reason) => {
-    return new Promise(function(resolve, reject) {
-        setTimeout(reject.bind(null, reason), RETRY_INTERVAL); 
-    });
-}
-
-// const waitAndReturnAgreeToItem = () => {
-//   const agreeToItem = document.querySelector("#agreeto-item")
-//   console.log("[waitAndReturnAgreeToItem] ", agreeToItem)
-//   if (!agreeToItem) {
-//     setTimeout(() => waitAndReturnAgreeToItem(), 1000)
-//   }
-//   return agreeToItem
-// }
-let attemptCount = 0
-const attempt = async () => {
-  attemptCount++
-  console.log("attempt ",attemptCount)
-  const agreeToItem = document.querySelector("#agreeto-item")
-  if (!agreeToItem) throw Error("Element with id #agreeto-item not found.")
-  return agreeToItem
-}
-export const getMountPoint = async () => {
-  observer.observe(document.body, { childList: true });
-  const MAX_RETRIES = 5;
-  let promise = Promise.reject();
-  let result: HTMLElement = null;
-  // for range
-  [...Array(MAX_RETRIES)].map((_,i) => {
-    promise = promise.catch(attempt).catch(rejectDelay)
-  });
-  promise = promise.then((res) => {console.log("res: ", res); result = res}).catch((err) => console.error(err))
-  // return inserted div to mount react
-  console.log("after awaited function")
-  return result
 }
 
 // REACT COMPONENT TO MOUNT
@@ -85,6 +28,61 @@ const GmailItem = () => {
       </button>
     </div>
   )
+}
+
+// CUSTOM getRootContainer INJECTION
+// note (richard): We want to prevail Gmail styling on our root container, so we provide plasmo with our own
+const CONTAINER_ID = "agreeto-item"
+export const getRootContainer = async () => {
+  let rootContainer; // to return later
+
+  // wait until the Gmail compose windows' bottom sheet(s) is inserted to the DOM 
+  const messageWindows = await waitForElements(".btC");
+  
+  // insert rootContainer
+  messageWindows.forEach((messageWindowHotbar) => {
+    if (!messageWindowHotbar.querySelector(`#${CONTAINER_ID}`)) {
+      // create container with our custom ID
+      const container = document.createElement("div");
+      container.id = CONTAINER_ID;
+
+      // insert next to send button
+      const sendButton = messageWindowHotbar.childNodes.item(0);
+      sendButton?.parentNode?.insertBefore(container, sendButton.nextSibling);
+      
+      // persist container outside of closure
+      rootContainer = container;
+    }
+  });
+  // ✅ return rootContainer to mount react onto
+  return rootContainer
+}
+
+// HELPER
+/**
+ * A helper function that awaits for a query selector to be inserted into the DOM
+ * 
+ * Note (richard): Inspired by https://stackoverflow.com/a/61511955/5608461
+ */
+const waitForElements = (selector) => {
+  return new Promise(resolve => {
+      if (document.querySelectorAll(selector)) {
+          return resolve(document.querySelectorAll(selector));
+      }
+
+      const observer = new MutationObserver(mutations => {
+        const target = mutations.addedNodes.find(node => node.matchesSelector(selector))
+          if (target) {
+              resolve(target);
+              observer.disconnect();
+          }
+      });
+
+      observer.observe(document.body, {
+          childList: true,
+          subtree: true
+      });
+  });
 }
 
 export default GmailItem
