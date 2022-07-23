f610fa56662410c908771553d2aa44002d99269d
p6l-richard <richard.poelderl@gmail.com>
2022-07-23 17:39:22 +0100

    refactor: add performance optimised waitForElement & add TODO: note to switch out consumer of waitForElements

--- contents/gmail.tsx
+++ contents/gmail.tsx
@@ -1,7 +1,7 @@
-import type { PlasmoContentScript } from "plasmo"
 import icon from "data-base64:~assets/icon512.png"
-import { ReactElement, useEffect } from "react";
-import { createRoot } from "react-dom/client";
+import type { PlasmoContentScript } from "plasmo"
+import { ReactElement, useEffect } from "react"
+import { createRoot } from "react-dom/client"
 
 export const config: PlasmoContentScript = {
   matches: ["https://mail.google.com/mail/u/0/*"] // test on: https://mail.google.com/mail/u/0/#inbox?compose=new
@@ -34,26 +34,27 @@ const GmailItem = () => {
 // note (richard): We want to prevail Gmail styling on our root container, so we provide plasmo with our own
 const CONTAINER_ID = "agreeto-item"
 export const getRootContainer = async () => {
-  let rootContainer; // to return later
+  let rootContainer // to return later
 
-  // wait until the Gmail compose windows' bottom sheet(s) is inserted to the DOM 
-  const messageWindows = await waitForElements(".btC");
-  
+  // wait until the Gmail compose windows' bottom sheet(s) is inserted to the DOM
+  // TODO: use performance-optimised waitForElement instead
+  const messageWindows = await waitForElements(".btC")
+
   // insert rootContainer
   messageWindows.forEach((messageWindowHotbar) => {
     if (!messageWindowHotbar.querySelector(`#${CONTAINER_ID}`)) {
       // create container with our custom ID
-      const container = document.createElement("div");
-      container.id = CONTAINER_ID;
+      const container = document.createElement("div")
+      container.id = CONTAINER_ID
 
       // insert next to send button
-      const sendButton = messageWindowHotbar.childNodes.item(0);
-      sendButton?.parentNode?.insertBefore(container, sendButton.nextSibling);
-      
+      const sendButton = messageWindowHotbar.childNodes.item(0)
+      sendButton?.parentNode?.insertBefore(container, sendButton.nextSibling)
+
       // persist container outside of closure
-      rootContainer = container;
+      rootContainer = container
     }
-  });
+  })
   // ✅ return rootContainer to mount react onto
   return rootContainer
 }
@@ -61,28 +62,72 @@ export const getRootContainer = async () => {
 // HELPER
 /**
  * A helper function that awaits for a query selector to be inserted into the DOM
- * 
+ *
  * Note (richard): Inspired by https://stackoverflow.com/a/61511955/5608461
  */
 const waitForElements = (selector) => {
-  return new Promise(resolve => {
-      if (document.querySelectorAll(selector)) {
-          return resolve(document.querySelectorAll(selector));
+  return new Promise<NodeListOf<HTMLElement>>((resolve) => {
+    if (document.querySelectorAll(selector)) {
+      return resolve(document.querySelectorAll(selector))
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      const target = mutations.addedNodes.find((node) =>
+        node.matchesSelector(selector)
+      )
+      if (target) {
+        resolve(target)
+        observer.disconnect()
       }
+    })
 
-      const observer = new MutationObserver(mutations => {
-        const target = mutations.addedNodes.find(node => node.matchesSelector(selector))
-          if (target) {
-              resolve(target);
-              observer.disconnect();
-          }
-      });
-
-      observer.observe(document.body, {
-          childList: true,
-          subtree: true
-      });
-  });
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    })
+  })
 }
+
+/**
+ * A helper function that awaits an HTML element to be inserted into the DOM
+ *
+ * Note (richard):
+ * API Inspired by https://stackoverflow.com/a/61511955/5608461
+ * Performance notes (inspired by https://stackoverflow.com/a/38882022/5608461):
+ * - on success, disconnect the observer & attach a new one (ideally, provide a `parent` param to attach it non-recursively by setting subtree: false)
+ * - if you can't use getElementById, prefer getElementsByTagName and getElementsByClassName and _avoid_ querySelector and especially the extremely slow querySelectorAll
+ * - If querySelectorAll is absolutely unavoidable inside MutationObserver callback, first perform the querySelector check, on the average such combo will be much faster.
+ * - avoid for loops (or array methods) inside the MutationObserver callback
+ */
+const waitForElement = (props: {id: string, parent?: HTMLElement} | {className: string, parent?: HTMLElement}) => {
+   const id = props?.id;
+   const className = props?.className;
+   return new Promise<HTMLElement>((resolve) => {
+    // if element already present, return
+    const earlyElement = id ? document.getElementById(id) : document.getByClassName(className);
+    if (earlyElement) {
+      return resolve(earlyElement)
+    }
+
+    // else observe dom mutations for our element
+    const observer = new MutationObserver((mutations) => {
+      // was the element added to the entire document?
+      const addedElement = id ? document.getElementById(id) : document.getByClassName(className);
+      if (addedElement) {
+        resolve(addedElement)
+        // always disconnect observer on success
+        observer.disconnect()
+      }
+    })
+
+    // attach a new non-recursive observer on a provided element (or document.body if none provided)
+    observer.observe(parent || document.body, {
+      childList: true,
+      // note (richard): Whenever possible observe direct parents non-recursively (subtree: false).
+      subtree: !Boolean(parent)
+    })
+  })
+}
+
 
 export default GmailItem
