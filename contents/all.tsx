a5dd73f4188d32bf266989dc2100094bab2a91cf
Stefan Aleksic <stefan@plasmo.com>
2022-08-03 15:08:18 +0100

    Getting modal to open on action

--- contents/all.tsx
+++ contents/all.tsx
@@ -7,19 +7,40 @@ export const config: PlasmoContentScript = {
   matches: ["https://*/*"]
 }
 
+const DIV_ID = "plasmo-shadow-div"
 const SHADOW_STYLE_ID = "shadow-styling"
+
+// We use the getRootContainer to inject the shadow host in our own way
+export const getRootContainer = async () => {
+  const container = document.createElement("div")
+
+  container.id = "plasmo-shadow-container"
+
+  container.style.cssText = `
+    display: none;
+    z-index: 1;
+    position: absolute;
+  `
+
+  // This is the div that the shadow host will be inserted into
+  const shadowHost = document.createElement("div")
+  shadowHost.id = DIV_ID
+
+  const shadowRoot = shadowHost.attachShadow({ mode: "open" })
+  document.body.insertAdjacentElement("beforebegin", shadowHost)
+
+  shadowRoot.appendChild(await getStyle())
+
+  shadowRoot.appendChild(container)
+  return container
+}
+
 export const getStyle = () => {
   const style = document.createElement("style")
   style.id = SHADOW_STYLE_ID
   style.textContent = cssText
   return style
 }
-// this let's us define the shadow host element's id plasmo inserts on the content pages
-// note (richard): we need access to this later in our `showModal` message handler to inverse visibility
-const SHADOW_HOST_ID = "all-pages-shadow-host"
-export const getShadowHostId = () => SHADOW_HOST_ID
-
-const SHADOW_CONTAINER_ID = "plasmo-shadow-container"
 // Handle action click in content-script
 onMessage("showModal", async ({ data }) => {
   // early return
@@ -44,37 +65,19 @@ onMessage("showModal", async ({ data }) => {
   // 🚫 -----
 
   // THIS WORKS AT RUNTIME but I can't define the initial state of the shadowHost.style (I want: display: none)
-  const plasmoShadowHost = document.getElementById(SHADOW_HOST_ID)
-  if (!plasmoShadowHost) {
-    alert(`shadow root not found!`)
-    return { success: false }
-  }
-  const shadowRoot = plasmoShadowHost.shadowRoot
-  if (!shadowRoot) {
-    alert(`Shadow host doesn't have a shadowRoot attached!`)
-    return { success: false }
-  }
 
-  // note (richard): cumbersomly remove the initial style tag display reference to enable toggling via `style` prop
-  const regex = /(#plasmo-shadow-container\s*{(?:(.|\n)*))(display:\s*none;)/
-  const styleContent = shadowRoot.getElementById(SHADOW_STYLE_ID)
-  const isHiddenInitially = Boolean(styleContent?.textContent?.match(regex)) // need to store ref before we remove
-  console.log("is hidden initially? " + isHiddenInitially, styleContent)
   // clean up the initial display: none from style element so that we can toggle
-  if (styleContent) {
-    styleContent.textContent?.replace(regex, "$1")
-  }
   // Now, we can inverse visibility
   // note (richard): `flex` is somewhat random here
-  const shadowContainer = shadowRoot.getElementById(SHADOW_CONTAINER_ID)
+  const shadowContainer = document
+    ?.getElementById(DIV_ID)
+    ?.shadowRoot?.getElementById("plasmo-shadow-container")
   if (!shadowContainer) {
     alert(`style element within shadowRoot not found!`)
     return { success: false }
   }
   shadowContainer.style.display =
-    shadowContainer.style.display === "none" || isHiddenInitially
-      ? "flex"
-      : "none"
+    shadowContainer.style.display === "none" ? "flex" : "none"
 
   return { success: true }
 })
